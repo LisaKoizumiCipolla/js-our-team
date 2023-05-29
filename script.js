7d0fcf157742f59c27b8c1aa04bc59502ad07f22
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-29 15:53:27 +0200

    Try to print in html

---
+++ script.js
@@ -0,0 +1,52 @@
+
+//Array team members
+const teamMembers = [
+    {
+        name : "Wayne Barnett",
+        position : "Founder & CEO",
+        img : "wayne-barnett-founder-ceo.jpg" ,
+    },
+    {
+        name : "Angela Caroll",
+        position : "Chief Editor",
+        img : "angela-caroll-chief-editor.jpg" ,
+    },
+    {
+        name : "Walter Gordon",
+        position : "Office Manager",
+        img : "walter-gordon-office-manager.jpg" ,
+    },
+    {
+        name : "Angela Lopez",
+        position : "Social Media Manager",
+        img : "angela-lopez-social-media-manager.jpg" ,
+    },
+    {
+        name : "Scott Estrada",
+        position : "Developer",
+        img : "scott-estrada-developer.jpg" ,
+    },
+    {
+        name : "Barbara Ramos",
+        position : "Graphic Designer",
+        img : "barbara-ramos-graphic-designer.jpg" ,
+    },
+]
+
+console.log(teamMembers);
+
+// creation link with html ul
+const teamMembersList = document.getElementsByClassName("ul");
+
+// creation variable for li item
+let teamMembersListItem;
+
+for (let i = 0 ; i < teamMembers.length ; i++){
+
+    teamMembersListItem = document.createElement("li");
+    teamMembersListItem.append(teamMembers[i]);
+    
+    console.log(teamMembers[i]);
+
+    teamMembersList.appendChild(teamMembersListItem);
+}
